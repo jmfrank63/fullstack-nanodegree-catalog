485a7c62697d382a7509813fe24ad9b6633b44c6
Johannes Frank <jmfrank63@gmail.com>
2015-05-28 19:35:42 +0000

    added project.py Hello World program

---
+++ project.py
@@ -0,0 +1,16 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 8080)
+
