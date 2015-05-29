8cf71a8160b467451ceb9fe2818504a60384406b
Johannes Frank <jmfrank63@gmail.com>
2015-05-29 01:08:46 +0000

    added database code

--- project.py
+++ project.py
@@ -3,12 +3,26 @@
 
 from flask import Flask
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+def create_session():
+    engine = create_engine('sqlite:///restaurantmenu.db')
+    Base.metadata.bind = engine
+
+    DBSession = sessionmaker(bind = engine)
+    return DBSession()
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/hello')
 def hello_world():
-    return 'Hello World!'
+    session = create_session()
+    restaurant = session.query(Restaurant).first()
+    output = restaurant.name
+    return output
 
 if __name__ == '__main__':
     app.debug = True
