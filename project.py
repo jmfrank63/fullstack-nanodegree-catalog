5808ad9ff79331fc3dd13e2927ba11f2909a92f0
Johannes Frank <jmfrank63@gmail.com>
2015-05-30 09:02:10 +0000

    added a template

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -17,12 +17,35 @@ def create_session():
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello')
-def hello_world():
+def index():
+    return 'It works!'
+
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurant_menu(restaurant_id):
     session = create_session()
-    restaurant = session.query(Restaurant).first()
-    output = restaurant.name
-    return output
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
+    # output = '<br><br>'.join(['<br>'.join([item.name,item.price,item.description]) for item in items])
+    return render_template('menu.html', restaurant = restaurant, items = items)
+
+# Task 1: Create route for newMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/edit/<int:menu_id>/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/delete/<int:menu_id>/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
 
 if __name__ == '__main__':
     app.debug = True
