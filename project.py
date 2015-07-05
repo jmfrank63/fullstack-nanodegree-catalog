d742402a4e45e16f61d27eee04699c7373b9538b
Johannes Frank <jmfrank63@gmail.com>
2015-07-05 20:22:31 +0000

    changed function name to pep8 standard

--- project.py
+++ project.py
@@ -31,21 +31,21 @@ session = DBSession()
 
 app = Flask(__name__)
 
-def createUser(login_session):
-    newUser = User(name=login_session['username'], email=login_session[
+def create_user(login_session):
+    new_user = User(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
-    session.add(newUser)
+    session.add(new_user)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
 
 
-def getUserInfo(user_id):
+def get_user_info(user_id):
     user = session.query(User).filter_by(id=user_id).one()
     return user
 
 
-def getUserID(email):
+def get_user_id(email):
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.id
